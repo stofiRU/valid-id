5463ff582359f8f00e7cf3d676f164a49016c64f
Kristófer Gudmundsson <kristofergudmundsson@tfw107212.skyrr.local>
2018-12-07 13:50:43 +0000

    test file added. checksum created

--- valid.py
+++ valid.py
@@ -2,6 +2,20 @@
 # a person born between 1900 and 2099; False otherwise. To determine whether the ID-number is valid or not, you 
 # have to use the checksum (english version) of the ID-number. Furthermore, the first six digits of the ID-number 
 # must represent a valid date.
+import datetime
 
 def valid(digits):
+    if len(digits) != 10:
+        return False
+    day = int(digits[:2])
+    month = int(digits[2:4])
+    year = int(digits[4:6])
+    random = int(digits[6:8])
+    # 1209972979
+    # C = 11 - ((3xD1 + 2xD2 + 7xM1 + 6xM2 + 5xY1 + 4xY2 + 3xR1 + 2xR2) mod 11)
+    checksum = 11 - (((3*int(digits[0:1])) + (2*int(digits[1:2])) + (7*int(digits[2:3])) + (6*int(digits[3:4])) + (5*int(digits[4:5])) + (4*int(digits[5:6])) + (3*int(digits[6:7])) + (2*int(digits[7:8]))) % 11)
+
+    # if(datetime.datetime(year, month, day)):
+    #     if(20 < random < 99):
     
+    return checksum
---
+++ valid_test.py
@@ -0,0 +1,22 @@
+# tests for valid.py
+from valid import valid
+
+print("T:",valid('1209972979'))
+
+# test for checksum for 1209972979:
+# print("(1) 3xD1: 1*3: 3:", (3*int(digits[0:1])))
+# print("(2) 2xD2: 2*2: 4:", (2*int(digits[1:2])))
+# print("(0) 7xM1: 7*0: 0:", (7*int(digits[2:3])))
+# print("(9) 6xM2: 6*9: 54:", (6*int(digits[3:4])))
+# print("(9) 5xY1: 5*9: 45:", (5*int(digits[4:5])))
+# print("(7) 4xY2: 4*7: 28:", (4*int(digits[5:6])))
+# print("(2) 3xR1: 3*2: 6:", (2*int(digits[7:8])))
+# print("(9) 2xR2: 2*9: 18:", (2*int(digits[8:9])))
+# print("1",(int(digits[0:1])))
+# print("2",(int(digits[1:2])))
+# print("0",(int(digits[2:3])))
+# print("9",(int(digits[3:4])))
+# print("9",(int(digits[4:5])))
+# print("7",(int(digits[5:6])))
+# print("2",(int(digits[6:7])))
+# print("9",(int(digits[7:8])))
