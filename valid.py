3d61d4cbd737bf6706425d4d374837a14ecbedc4
Kristófer Gudmundsson <kristofergudmundsson@tfw107212.skyrr.local>
2018-12-07 10:58:23 +0000

    function created

---
+++ valid.py
@@ -0,0 +1,7 @@
+# Function valid takes a string of 10 digits. The function returns True if the ID-numbers is a valid ID-number of 
+# a person born between 1900 and 2099; False otherwise. To determine whether the ID-number is valid or not, you 
+# have to use the checksum (english version) of the ID-number. Furthermore, the first six digits of the ID-number 
+# must represent a valid date.
+
+def valid(digits):
+    
